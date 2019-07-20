0140dc6f489339f389b634878691f76871c57bb7
cxbsoft <admincai@cxbsoft.top>
2019-07-20 23:43:43 +0800

    The first version

---
+++ IoT_Website/header.php
@@ -0,0 +1,12 @@
+<!--<link rel="stylesheet" href="//cdnjs.loli.net/ajax/libs/mdui/0.4.3/css/mdui.min.css" />
+<script src="//cdnjs.loli.net/ajax/libs/mdui/0.4.3/js/mdui.min.js"></script>-->
+<link rel="stylesheet" href="css/mdui.min.css"/>
+<script src="js/mdui.min.js"></script>
+<script src="../js/mdui.min.js"></script>
+<link rel="shortcut icon" href=" 123.ico" />
+<script src="js/mod.js"></script>
+<link rel="stylesheet" href="css/footer.css"/>
+<div id="footer">
+    Copyright © 2019 <a href="//cxbsoft.top">cxbsoft</a
+>.All rights reserved 
+</div>
---
+++ IoT_Website/user/mysql.php
@@ -0,0 +1,5 @@
+<?php
+
+    $db = mysqli_connect("localhost","arduino","wabadmin1","arduino")
+
+?>
